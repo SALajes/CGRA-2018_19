34742f634bc8efb3c447d10e1fd3dfcf40685dba
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-05-16 17:04:13 +0100

    Added MyTerrain and textures/shaders for it. Deleted MyFloor which won't be necessary

--- projB/MyScene.js
+++ projB/MyScene.js
@@ -23,7 +23,7 @@ class MyScene extends CGFscene {
 
         //Initialize scene objects
         this.axis = new CGFaxis(this);
-        this.plane = new Plane(this, 32);
+        this.plane = new MyTerrain(this);
         this.house = new MyHouse(this);
         this.map = new MyCubeMap(this);
         this.bird = new MyBird(this);
@@ -101,6 +101,7 @@ class MyScene extends CGFscene {
         // this.scale(60, 60, 1);
         // this.plane.display();
         // this.popMatrix();
+        this.plane.display();
 
         this.bird.display();
 
---
+++ projB/MyTerrain.js
@@ -0,0 +1,40 @@
+class MyTerrain extends CGFobject{
+	constructor(scene) {
+		super(scene);
+        this.plane = new Plane(scene, 32);
+        this.shader = new CGFshader(scene.gl, "shaders/terrain.vert", "shaders/terrain.frag")
+        this.terrain = new CGFtexture(scene, "images/terrain.jpg");
+        this.heightMap = new CGFtexture(scene, "images/myHeightmap.jpg");
+        this.appearance = new CGFappearance(scene);
+		this.appearance.setAmbient(0.3, 0.3, 0.3, 1);
+		this.appearance.setDiffuse(0.7, 0.7, 0.7, 1);
+		this.appearance.setSpecular(0.0, 0.0, 0.0, 1);
+        this.appearance.setShininess(120);
+        this.appearance.setTexture(this.terrain);
+		this.appearance.setTextureWrap('REPEAT', 'REPEAT');
+        
+        this.shader.setUniformsValues({ uSampler1: 1 });
+		this.shader.setUniformsValues({ uSampler2: 2 });
+    }
+
+    display(){
+        this.appearance.apply();
+
+        this.scene.setActiveShader(this.shader);
+        this.scene.pushMatrix();
+
+        this.terrain.bind(1);
+        this.heightMap.bind(2);
+
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_WRAP_S, this.scene.gl.REPEAT);
+        this.scene.gl.texParameteri(this.scene.gl.TEXTURE_2D, this.scene.gl.TEXTURE_WRAP_T, this.scene.gl.REPEAT);
+        
+        this.scene.pushMatrix();
+        this.scene.rotate(-0.5*Math.PI, 1, 0, 0);
+        this.scene.scale(60, 60, 1);
+        this.plane.display();
+        this.scene.popMatrix();
+
+        this.scene.setActiveShader(this.scene.defaultShader);
+    }
+}
