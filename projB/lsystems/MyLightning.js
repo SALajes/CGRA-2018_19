e97874459cdfc5541448fdc5c036bc413a96ba39
CajoAlbuquerque <carlosjo.da@gmail.com>
2019-05-30 23:36:10 +0100

    Started animation process

--- projB/lsystems/MyLightning.js
+++ projB/lsystems/MyLightning.js
@@ -12,14 +12,12 @@ class MyLightning extends MyLSystem {
         this.iterations = 3;
         this.scaleFactor = 0.5;
     }
-
     initGrammar() {
         this.grammar = {
-            "F": new MyRectangle(this.scene, 0.2, 1),
-            "X": new MyRectangle(this.scene, 0.2, 1)
+            "F": new MySegment(this.scene),
+            "X": new MySegment(this.scene)
         };
     }
-
     doGenerate() {
         this.initValues();
         super.generate(
@@ -29,18 +27,89 @@ class MyLightning extends MyLSystem {
                 "X": [ "F[-X][X]F[-X]+FX",
                         "[X+X+X--FX]+F-X[F-X+X]",
                         "[X-X-X]FF+X--X[+X+F]",
-                        "[-x]X[+X]" ]
+                        "[-X]X[+X]",
+                        "F-X+X+XF" ]
             },
             this.angle,
             this.iterations,
             this.scaleFactor
         );
     }
-
     display() {
         this.scene.pushMatrix();
+        this.scene.translate(0, 10, 0);
         this.scene.scale(1.0,-1.0,1.0);
         super.display();
+        this.scene.popMatrix();
+    }
+    update(t) {
+        this.elapsedTime = t - this.startTime;
+        this.depth = (this.axiom.length - this.depth) / this.elapsedTime;
+    }
+    startAnimation(t) {
+        super.iterate();
+        this.depth = 0;
+        this.startTime = t;
+    }
+    display(){
+        this.scene.pushMatrix();
+        this.scene.scale(this.scale, this.scale, this.scale);
+
+        var i;
+
+        // percorre a cadeia de caracteres
+        for (i=0; i<this.depth; ++i){
+
+            // verifica se sao caracteres especiais
+            switch(this.axiom[i]){
+                case "+":
+                    // roda a esquerda
+                    this.scene.rotate(this.angle, 0, 0, 1);
+                    break;
+
+                case "-":
+                    // roda a direita
+                    this.scene.rotate(-this.angle, 0, 0, 1);
+                    break;
+
+                case "[":
+                    // push
+                    this.scene.pushMatrix();
+                    break;
+
+                case "]":
+                    // pop
+                    this.scene.popMatrix();
+                    break;
+
+                case "\\":
+                    this.scene.rotate(this.angle, 1, 0, 0);
+                    break;
+
+                case "/":
+                    this.scene.rotate(-this.angle, 1, 0, 0);
+                    break;
+
+                case "^":
+                    this.scene.rotate(this.angle, 0, 1, 0);
+                    break;
+
+                case "&":
+                    this.scene.rotate(-this.angle, 0, 1, 0);
+                    break;
+
+                // processa primitiva definida na gramatica, se existir
+                default:
+                    var primitive=this.grammar[this.axiom[i]];
+
+                    if ( primitive )
+                    {
+                        primitive.display();
+                        this.scene.translate(0, 1, 0);
+                    }
+                    break;
+            }
+        }
         this.scene.popMatrix();
     }
 }
---
+++ projB/lsystems/MySegment.js
@@ -0,0 +1,21 @@
+class MySegment extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.quad = new MyQuad(scene);
+        this.initMaterials(scene);
+    }
+    initMaterials(scene) {
+        this.material = new CGFappearance(scene);
+        this.material.setAmbient(1.0, 1.0, 1.0, 1.0);
+        this.material.setDiffuse(0.5, 0.5, 0.5, 1.0);
+        this.material.setSpecular(0.5, 0.5, 0.5, 1.0);
+        this.material.setShininess(2.0);
+    }
+    display() {
+        this.material.apply()
+        this.scene.pushMatrix();
+        this.scene.scale(0.1,1.3,1.0);
+        this.quad.display();
+        this.scene.popMatrix();
+    }
+}
